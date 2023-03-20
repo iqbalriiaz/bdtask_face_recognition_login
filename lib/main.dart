dfb0e61dcb75d1d2da504a5af5788118f5391e5d
iqbalriiaz <iqbalriiaz@gmail.com>
2023-03-20 14:33:40 +0600

    pushing the whole project

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
